9512748b6e4efbef33ae2754734bbc4dcc278f08
mb961107 <mirko.blazina@convergent-usa.com>
2021-05-23 15:06:06 +0200

    diagram fix, header lower height, click listener to go to first slide

--- script/main.js
+++ script/main.js
@@ -1,76 +1,78 @@
-let page = $('.wrapper');
-let slider = $('.slick');
+let page = $(".wrapper");
+let slider = $(".slick");
 let pageEnd = false;
-const nav = document.querySelector('nav');
+const nav = document.querySelector("nav");
 slider.slick({
-    vertical: true,
-    verticalSwiping: true,
-    slidesToScroll: 1,
-    slidesToShow: 1,
-    infinite: false,
-    slide: 'section',
-    arrows: true,
-    prevArrow: $('.slick-prev'),
-    nextArrow: $('.slick-next'),
-    dots: true,
-    appendDots: $('.slick-vertical-dots'),
+  vertical: true,
+  verticalSwiping: true,
+  slidesToScroll: 1,
+  slidesToShow: 1,
+  infinite: false,
+  slide: "section",
+  arrows: true,
+  prevArrow: $(".slick-prev"),
+  nextArrow: $(".slick-next"),
+  dots: true,
+  appendDots: $(".slick-vertical-dots"),
 });
 
-slider.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-    //console.log('next: ' + nextSlide)
+slider.on("beforeChange", function (event, slick, currentSlide, nextSlide) {
+  //console.log('next: ' + nextSlide)
 });
 
-slider.on('afterChange', function (event, slick, currentSlide, nextSlide) {
-    /* console.log(currentSlide); */
-    if(slick.$slides[currentSlide].classList.contains('nav-black')){
-        if(!nav.classList.contains('nav-black')){
-            nav.classList.add('nav-black')
-        }
-    }else{
-        if(nav.classList.contains('nav-black')){
-            nav.classList.remove('nav-black')
-        }
+slider.on("afterChange", function (event, slick, currentSlide, nextSlide) {
+  /* console.log(currentSlide); */
+  if (slick.$slides[currentSlide].classList.contains("nav-black")) {
+    if (!nav.classList.contains("nav-black")) {
+      nav.classList.add("nav-black");
     }
-    const header = document.querySelector('header');
-    if (currentSlide == slick.slideCount - 1 && !pageEnd) {
-        pageEnd = true;
+  } else {
+    if (nav.classList.contains("nav-black")) {
+      nav.classList.remove("nav-black");
     }
-    if(currentSlide !== slick.slideCount - 1 && pageEnd){
-        pageEnd = false;
-        page.animate({ "top": "0px" }, "normal");
-    }
-    if(currentSlide > 0){
-        header.classList.add('closed')
-    }else if(currentSlide == 0){
-        header.classList.remove('closed')
-    }
-    console.log(slick.$slides[currentSlide].classList)
+  }
+  const header = document.querySelector("header");
+  if (currentSlide == slick.slideCount - 1 && !pageEnd) {
+    pageEnd = true;
+  }
+  if (currentSlide !== slick.slideCount - 1 && pageEnd) {
+    pageEnd = false;
+    page.animate({ top: "0px" }, "normal");
+  }
+  if (currentSlide > 0) {
+    header.classList.add("closed");
+  } else if (currentSlide == 0) {
+    header.classList.remove("closed");
+  }
 });
 
-page.on('wheel', function (e) {
-    /* console.log("start", pageEnd); */
-    e.preventDefault();
-    if (e.originalEvent.deltaY < 0) {
-        if (pageEnd) {
-            page.clearQueue();
-            page.animate({ "top": "0px" }, "normal");
-            pageEnd = false;
-            /* console.log("end", pageEnd); */
-            return;
-        }
-        slider.slick('slickPrev');
-    } else {
-        if (pageEnd) {
-            page.clearQueue();
-            page.animate({ "top": "-" + $('footer').height() + "px" }, "normal");
-            /* console.log("end", pageEnd); */
-            return;
-        }
-        slider.slick('slickNext');
+page.on("wheel", function (e) {
+  /* console.log("start", pageEnd); */
+  e.preventDefault();
+  if (e.originalEvent.deltaY < 0) {
+    if (pageEnd) {
+      page.clearQueue();
+      page.animate({ top: "0px" }, "normal");
+      pageEnd = false;
+      /* console.log("end", pageEnd); */
+      return;
     }
-    /* console.log("end", pageEnd); */
+    slider.slick("slickPrev");
+  } else {
+    if (pageEnd) {
+      page.clearQueue();
+      page.animate({ top: "-" + $("footer").height() + "px" }, "normal");
+      /* console.log("end", pageEnd); */
+      return;
+    }
+    slider.slick("slickNext");
+  }
+  /* console.log("end", pageEnd); */
 });
 
-if(window.innerHeight < 768) {
-   // slider.slick('unslick');
+const logo = document.querySelectorAll(".logo");
+for (let i = 0; i < logo.length; i++) {
+  logo[i].addEventListener("click", function () {
+    slider.slick("slickGoTo", 0);
+  });
 }
